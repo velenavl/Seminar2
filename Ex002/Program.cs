db924250f25fd5ed6ce6496ad1e719be8def8be6
Elena <velenavl@inbox.ru>
2022-09-03 21:34:01 +0300

    Ex002

---
+++ Ex002/Program.cs
@@ -0,0 +1,14 @@
+﻿// Программа, которая выводит случайное трёхзначное число и удаляет вторую цифру числа.
+
+Random random = new Random();
+
+int randomNumber = random.Next(100,1000);
+
+Console.WriteLine(randomNumber);
+
+int firstNumber = randomNumber / 100;
+int lastNumber = randomNumber % 10;
+
+int result = (firstNumber * 10) + lastNumber;
+Console.Write(result);
+
