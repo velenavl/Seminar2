9b54fac863129225a88054d6f1fafb7d91728b87
Elena <velenavl@inbox.ru>
2022-09-03 22:17:21 +0300

    Ex004

---
+++ Ex004/Program.cs
@@ -0,0 +1,24 @@
+﻿// Метод
+Random random = new Random();
+
+int randomNumber = random.Next(10,100);
+
+Console.WriteLine(randomNumber);
+
+int GetMaxNumber(int number)
+{
+    int lastNumber = number % 10;
+    int firstNumber = number / 10;
+    if (lastNumber > firstNumber)
+    {
+        return lastNumber;
+    }
+    else
+    {
+        return firstNumber;
+    }
+}
+
+int result = GetMaxNumber(randomNumber);
+
+Console.WriteLine(result);
