c76e3790f511fdb73c04f6168c8e991a719d86b9
Elena <velenavl@inbox.ru>
2022-09-04 17:30:17 +0300

    HomeWork2

---
+++ HomeWork2/HW10/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+Console.Write("Введите число: ");
+int Number = Convert.ToInt32(Console.ReadLine());
+int ShowSecondNumber(int number)
+{
+    int firstNumber = number * 0;
+    int secondNumber = number % 100;
+    int result = (secondNumber / 10);
+    {
+        return result;
+    }
+}
+int result = ShowSecondNumber(Number);
+Console.WriteLine(result);
