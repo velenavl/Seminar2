65d6370d17f55f0de80d0e9268b1499296ab1316
Elena <velenavl@inbox.ru>
2022-09-04 20:44:52 +0300

    HW15

---
+++ HomeWork2/HW15/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели,
+//  и проверяет, является ли этот день выходным.
+
+Console.Write("Введи цифру, обозначающую день недели: ");
+int dayNumber = Convert.ToInt32(Console.ReadLine());
+
+void CheckWeekday(int dayNumber)
+{
+    if (dayNumber == 6 || dayNumber == 7)
+    {
+        Console.WriteLine("Этот день выходной? - Да");
+    }  
+    else Console.WriteLine("Этот день выходной? - Нет");
+}
+
+CheckWeekday(dayNumber);
